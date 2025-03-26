7a728e9ad84c59c51c816659629eaf1a8ce4fca4
Hatef Eidi <hatef.121.h@gmail.com>
2025-03-26 14:17:49 +0000

    returning one episode using the makingEpisodeForThePage function

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 import { getOneEpisode, getAllEpisodes } from "./episodes.js"; // Import functions
 
 function setup() {
-  const allEpisodes = getAllEpisodes();
+  const oneEpisode = getOneEpisode();
 
-  // console.log(allEpisodes);
-  makePageForEpisodes(allEpisodes);
+  console.log(oneEpisode);
+  makePageForEpisodes(oneEpisode);
 }
 // {/* <template id="film-card">
 //         <section>
@@ -14,7 +14,14 @@ function setup() {
 //           <p description>About the film</p>
 //         </section>
 //       </template> */}
+
+const rootElem = document.getElementById("root");
+
+
 const filmCard = document.getElementById("film-card").content.cloneNode(true);
+
+function makePageForEpisodes(episodeList) {
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
 //Film Title, h3
 const filmTitle = filmCard.querySelector("h3");
@@ -34,38 +41,9 @@ filmImage.classList.add("film-image");
 //Appending film card tot he body
 
 filmCard.append(filmTitle, filmImage,filmDescription)
-document.body.appendChild(filmCard);
-
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+rootElem.appendChild(filmCard);
+  
 }
 
-// export function getOneEpisode() {
-//   return {
-//     id: 4952,
-//     url:
-//       "http://www.tvmaze.com/episodes/4952/game-of-thrones-1x01-winter-is-coming",
-//     name: "Winter is Coming",
-//     season: 1,
-//     number: 1,
-//     airdate: "2011-04-17",
-//     airtime: "21:00",
-//     airstamp: "2011-04-18T01:00:00+00:00",
-//     runtime: 60,
-//     image: {
-//       medium:
-//         "http://static.tvmaze.com/uploads/images/medium_landscape/1/2668.jpg",
-//       original:
-//         "http://static.tvmaze.com/uploads/images/original_untouched/1/2668.jpg",
-//     },
-//     summary:
-//       "<p>Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage.</p>",
-//     _links: {
-//       self: {
-//         href: "http://api.tvmaze.com/episodes/4952",
-//       },
-//     },
-//   };
-// }
+
 window.onload = setup;
